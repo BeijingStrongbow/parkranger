da6dc3b0fae52156ab48bfdfe1dca005e02ae6fe
giangpro93 <giangnt93@ku.edu>
2018-04-21 22:10:18 -0500

    Big update

--- app/src/main/java/com/github/beijingstrongbow/parkranger/CreateJoin.java
+++ app/src/main/java/com/github/beijingstrongbow/parkranger/CreateJoin.java
@@ -1,5 +1,7 @@
 package com.github.beijingstrongbow.parkranger;
 
+import android.app.ApplicationErrorReport;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,12 +27,19 @@ public class CreateJoin extends AppCompatActivity {
         joinbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                double groupid;
-                if (groupidtxt.getText().toString().length()!=0) {
-                    groupid = Double.parseDouble(groupidtxt.getText().toString());
-                    Toast.makeText(CreateJoin.this, "Joined Successfully",Toast.LENGTH_SHORT).show();
+                String name = nametxt.getText().toString();
+                if (name.length()==0)
+                    Toast.makeText(CreateJoin.this, "Invalid name", Toast.LENGTH_SHORT).show();
+                else {
+                    double groupid;
+                    if (groupidtxt.getText().toString().length() != 0) {
+                        groupid = Double.parseDouble(groupidtxt.getText().toString());
+                        Toast.makeText(CreateJoin.this, "Joined Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(CreateJoin.this, MapsActivity.class);
+                        startActivity(intent);
+                    } else
+                        Toast.makeText(CreateJoin.this, "Invalid group ID", Toast.LENGTH_SHORT).show();
                 }
-                else Toast.makeText(CreateJoin.this, "Invalid group ID",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -45,6 +54,8 @@ public class CreateJoin extends AppCompatActivity {
                     if (groupidtxt.getText().toString().length() != 0) {
                         groupid = Double.parseDouble(groupidtxt.getText().toString());
                         Toast.makeText(CreateJoin.this, "Created Successfully",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(CreateJoin.this,MapsActivity.class);
+                        startActivity(intent);
                     }
                     else
                         Toast.makeText(CreateJoin.this, "Invalid group ID", Toast.LENGTH_SHORT).show();
